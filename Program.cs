1a643f1e153eb2ab4c3915e238ca2dc7705474d1
Bahulya <sarvalabahulya456@gmail.com>
2021-11-18 21:35:38 +0530

    Linkedlist

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedlistUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                List<int> l1 = new List<int>();
+                l1.Add(70);
+                l1.Add(30);
+                l1.Add(56);
+                foreach (var ele in l1)
+                {
+                    Console.WriteLine(ele);
+                }
+            }
+        }
+    }
+}
+    
+
