7d0cf265814f27acf8de2857a5d207962becc420
Bahulyasarvala <91846471+Tutorials-cell@users.noreply.github.com>
2021-12-02 14:36:42 +0530

    Stack

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackUC2
+{
+    class Program
+    {
+       
+            static void Main(string[] args)
+            {
+                Stack<int> s1 = new Stack<int>();
+
+                s1.Push(56);
+                s1.Push(30);
+                s1.Push(70);
+                foreach (var element in s1)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("After---------------");
+                while (s1.Count != 0)
+                {
+                    Console.WriteLine(s1.Peek());
+                    s1.Pop();
+
+                }
+            }
+        }
+    }
+
